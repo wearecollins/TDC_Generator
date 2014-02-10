04b6eb1d6cda8b3f4ef15bf4929d39ff116b01d7
Brett Renfer <brett@robotconscience.com>
2014-02-10 10:58:07 -0500

    Added quick addons clone script

---
+++ cloneaddons.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Dependencies for RGBDToolkit
+
+#make sure you are in the TDC folder when you run this.
+
+cd ../../addons
+
+
+PREFIX="git clone https://github.com/"
+
+${PREFIX}labatrockwell/ofxLabFlexParticleSystem.git
+${PREFIX}rezaali/ofxUI.git
